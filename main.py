7322edab8140104efbfaba1615ad4b07accf2761
Damian <gardanw@gmail.com>
2022-05-11 20:54:41 +0200

    add method endpoint

--- main.py
+++ main.py
@@ -8,5 +8,31 @@ def root():
     return {"start": "1970-01-01"}
 
 
+@app.get("/method")
+def get_method():
+    return {"method": "GET"}
+
+
+@app.post("/method")
+def get_method():
+    return {"method": "POST"}
+
+
+@app.put("/method")
+def get_method():
+    return {"method": "PUT"}
+
+
+@app.options("/method")
+def get_method():
+    return {"method": "OPTIONS"}
+
+
+@app.delete("/method")
+def get_method():
+    return {"method": "DELETE"}
+
+
+
 class HerokuApp:
     app_url = "https://z1-d-jak-deployment-gardanw.herokuapp.com/"  # Fill your heroku app url here
