7d0414ea5a0207f808521cde6c19622f0067e35c
Damian <gardanw@gmail.com>
2022-05-11 20:28:39 +0200

    start

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"start": "1970-01-01"}
+
+
 class HerokuApp:
-    app_url = ""  # Fill your heroku app url here
+    app_url = "https://z1-d-jak-deployment-gardanw.herokuapp.com/"  # Fill your heroku app url here
